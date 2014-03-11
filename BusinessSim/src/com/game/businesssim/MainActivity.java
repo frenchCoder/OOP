2f0ffb56a9e75a631ae0288bc02898b941490345
icarreras <ivette.carreras@knights.ucf.edu>
2014-03-11 15:57:00 -0400

    Game Mainpage empty and framework for GameController
    This is the framework for the GameController. It contains the tabs and
    the fragments for each tab. Also the MainActivity is included.

--- BusinessSim/src/com/game/businesssim/MainActivity.java
+++ BusinessSim/src/com/game/businesssim/MainActivity.java
@@ -1,5 +1,5 @@
 package com.game.businesssim;
-
+// this is a test
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().hide();
     }
 
 
