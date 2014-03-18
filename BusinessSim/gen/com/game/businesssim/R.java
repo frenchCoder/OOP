f1d9f9de1b64c4c39face6cd52c746e23a102a97
icarreras <ivette.carreras@knights.ucf.edu>
2014-03-18 17:13:30 -0400

    Fixing issues in the merge

--- BusinessSim/gen/com/game/businesssim/R.java
+++ BusinessSim/gen/com/game/businesssim/R.java
@@ -228,27 +228,38 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f070001;
         public static final int app_name=0x7f070000;
-        public static final int cups=0x7f07000f;
+        public static final int cancel=0x7f070014;
+        public static final int closed=0x7f070015;
+        public static final int cups=0x7f07001a;
+        public static final int day_summary=0x7f07000f;
         /**   MainMenu strings 
          */
-        public static final int dialog_title=0x7f07000c;
-        public static final int drawer_close=0x7f070012;
-        public static final int drawer_open=0x7f070011;
-        public static final int ice=0x7f070010;
+        public static final int dialog_title=0x7f070017;
+        public static final int drawer_close=0x7f07001d;
+        public static final int drawer_open=0x7f07001c;
+        public static final int game_loaded=0x7f07000b;
+        public static final int game_not_saved=0x7f070013;
+        public static final int game_over=0x7f070016;
+        public static final int game_saved=0x7f07000a;
+        public static final int ice=0x7f07001b;
         public static final int label_status_money=0x7f070005;
         public static final int label_status_time=0x7f070007;
         /**  stand fragment strings 
          */
-        public static final int lemons=0x7f07000d;
-        public static final int pause_glossary=0x7f07000a;
-        public static final int pause_howto=0x7f07000b;
-        public static final int pause_save=0x7f070009;
+        public static final int lemons=0x7f070018;
+        public static final int ok=0x7f070012;
+        public static final int ok_to_exit=0x7f070011;
+        public static final int pause_glossary=0x7f07000d;
+        public static final int pause_howto=0x7f07000e;
+        public static final int pause_save=0x7f07000c;
         public static final int status_money=0x7f070006;
         public static final int status_time=0x7f070008;
-        public static final int sugar=0x7f07000e;
+        public static final int sugar=0x7f070019;
+        public static final int summary_title=0x7f070009;
         public static final int title_bank=0x7f070004;
         public static final int title_market=0x7f070003;
         public static final int title_stand=0x7f070002;
+        public static final int yes=0x7f070010;
     }
     public static final class style {
         /** 
---
+++ BusinessSim/src/com/game/businesssim/MainActivity.java
@@ -0,0 +1,93 @@
+package com.game.businesssim;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.DialogFragment;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements PlayDialogFragment.PlayOptionListener{
+
+	public final static String EXTRA_MESSAGE = "com.game.businesssim.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        getActionBar().hide();
+        
+        
+        final Button button_play = (Button)findViewById(R.id.chooseGameBtn);
+        final Button button_howto = (Button)findViewById(R.id.howtoBtn);
+        final Button button_glossary = (Button)findViewById(R.id.glossaryBtn);
+        final Button button_scores = (Button)findViewById(R.id.highscoreBtn);
+        
+        // Have all the buttons objects initialized
+        
+        button_play.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		showPlayDialog();
+        	}
+        	
+        });
+        
+        button_howto.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		// create the intent and start the GameControllerActivity
+        		Intent intent = new Intent(MainActivity.this, HowToActivity.class);
+        		startActivity(intent);	
+        	}
+        	
+        });
+        
+        button_glossary.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		// create the intent and start the GameControllerActivity
+        		Intent intent = new Intent(MainActivity.this, GlossaryActivity.class);
+        		startActivity(intent);	
+        	}
+        	
+        });
+        
+        button_scores.setOnClickListener(new OnClickListener(){
+        	@Override
+        	public void onClick(View v){
+        		// create the intent and start the GameControllerActivity
+        		Intent intent = new Intent(MainActivity.this, HighScoreActivity.class);
+        		startActivity(intent);	
+        	}
+        	
+        });
+        
+    }
+    
+    public void showPlayDialog(){
+    	DialogFragment dialog = new PlayDialogFragment();
+    	dialog.show(this.getFragmentManager(), "PlayOptionFragment");
+    }
+    
+
+	// this implements the PlayOptionListener interface
+    public void onOptionClick(int which){
+    	// Start the GameController Activity and pass in the option from the user
+    	Intent intent = new Intent(MainActivity.this, GameControllerActivity.class);
+    	String value = Integer.toString(which);
+    	intent.putExtra(EXTRA_MESSAGE, value);
+    	
+    	startActivity(intent);
+    }
+
+// This is not needed. The action bar is displayed but not the action overflow icon
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+    
+}
